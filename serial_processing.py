f83b11b84f918e45a41660c49a6e61c3755df891
faizat0506 <159166162+faizat0506@users.noreply.github.com>
2025-04-17 17:03:51 +0100

    serial_processing.py

--- serial_processing.py
+++ serial_processing.py
@@ -2,13 +2,13 @@ import pandas as pd
 import time
 
 # Load data
-data = pd.read_csv('Trips_by_Distance.csv')
+data = pd.read_csv('trips_by_distance.csv')
 
 # Start timer
 start_time = time.time()
 
 # Data processing logic
-result = data.groupby('column').sum()
+result = data.groupby('population Not Staying at Home').sum()
 
 # End timer
 end_time = time.time()
